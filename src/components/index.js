b54ebf705eef67163d778ea821bddef42bb9b1fb
bilal010 <bilal.scorpio123@gmail.com>
2022-10-11 01:23:38 +0500

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Article } from "./articles/article";
+export { default as Brand } from "./brands/brands";
+export { default as CTA } from "./cta/CTA";
+export { default as Feature } from "./features/features";
+export { default as Navbar } from "./navbar/navbar";
